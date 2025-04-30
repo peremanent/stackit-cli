e8537eda5afba833b10eba83e147ab976bf1a8c6
Pere Manent <pere.manent@mail.schwarz>
2025-04-30 12:28:58 +0200

    Add stackit git list instances command

---
+++ internal/cmd/git/git.go
@@ -0,0 +1,26 @@
+package git
+
+import (
+	"github.com/stackitcloud/stackit-cli/internal/cmd/git/instance/list"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/args"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/print"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/utils"
+
+	"github.com/spf13/cobra"
+)
+
+func NewCmd(p *print.Printer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "git",
+		Short: "Provides functionality for STACKIT Git",
+		Long:  "Provides functionality for STACKIT Git.",
+		Args:  args.NoArgs,
+		Run:   utils.CmdHelp,
+	}
+	addSubcommands(cmd, p)
+	return cmd
+}
+
+func addSubcommands(cmd *cobra.Command, p *print.Printer) {
+	cmd.AddCommand(list.NewCmd(p))
+}
